15d8ea1c8bb445c42e547401a8aacdddecb9a459
Kuba Łopusiński <siemakuba@gmail.com>
2015-03-19 14:46:11 +0100

    chane extending order

--- lib/mixes/extender.rb
+++ lib/mixes/extender.rb
@@ -1,8 +1,8 @@
 module Mixes
   module Extender
     def perform(target, source)
-      target.send(:include, source)
       target.send(:extend, source.const_get(:ClassMethods)) if source.const_defined?(:ClassMethods)
+      target.send(:include, source)
     end
     module_function :perform
   end
